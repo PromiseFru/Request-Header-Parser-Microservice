0ed9ca232ccc7d6f178fbc01665d1097fbd04c9e
Promise Fru <promisefru1@gmail.com>
2020-09-05 11:44:48 +0100

    Proj complete ...

--- app.js
+++ app.js
@@ -6,4 +6,12 @@ app.get("/", (req, res) => {
     res.send("hello world");
 })
 
+app.get("/api/whoami", (req, res) => {
+    var ipAddress = req.ip;
+    var language = req.acceptsLanguages();
+    var software = req.headers['user-agent'];
+
+    res.json({"ipaddress": ipAddress, "language": language, "software": software});
+})
+
 app.listen(port, console.log(`Listening on port ${port}`));
